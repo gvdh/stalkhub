599235f0d1b1e53204680d2057921690288746f2
Lucie Keunen <lucie@keunen.net>
2017-12-06 14:19:07 +0100

    Fixed bug in policy scope of Results (need to join providers table to find result's user)

--- app/policies/result_policy.rb
+++ app/policies/result_policy.rb
@@ -1,7 +1,7 @@
 class ResultPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.where(user: user)
+      scope.joins(:provider).where(providers: { user: user })
     end
   end
 end
