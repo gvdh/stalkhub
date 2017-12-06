bd7cc75515577fc5eeb1e1bce1a6277c51822e8e
Lucie Keunen <lucie@keunen.net>
2017-12-06 13:08:27 +0100

    Refactorization of results index (only one action, but with scopes)

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,20 +1,20 @@
 class ResultsController < ApplicationController
 
-  def fb_index
-    @results = policy_scope(Result).order(created_at: :desc)
-  end
+  def index
+    provider = Provider.where(name: params[:provider], user: current_user).first
+    if provider.nil?
+      flash[:error] = 'No provider'
+      redirect_to root_path
+    end
 
-  def fb_index_photo
-    @results = policy_scope(Result).order(created_at: :desc)
-    @photos = []
-    @results.each do |result|
-      @photos << result if result.category == "photo"
+    @results = policy_scope(Result).where(provider: provider).order(created_at: :desc)
+    if params[:type] == 'photo'
+      @results = @results.photos
+    elsif params[:type] == 'text'
+      @results = @results.texts
+    elsif params[:type] == 'video'
+      @results = @results.videos
     end
   end
 
-  def fb_index_text
-  end
-
-  def fb_index_video
-  end
 end
