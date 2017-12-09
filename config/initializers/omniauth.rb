9a34079cb5cb71d55cac2accf3dbc9b413d7d0a5
Lucie Keunen <31739527+luciekeunen@users.noreply.github.com>
2017-12-09 13:31:17 +0100

    Update omniauth.rb
    removed user_birthday and user_tagged_places from FB permissions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FB_ID'], ENV['FB_SECRET'],
-    scope: 'email,user_birthday,public_profile,user_photos,user_posts,user_tagged_places,user_videos, user_likes'
+    scope: 'email,public_profile,user_photos,user_posts,user_videos, user_likes'
 end
