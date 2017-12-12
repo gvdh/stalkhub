68d832c9e570790b0f6e4a28602865399437d707
G VDH <vanderhaegen.gauthier@gmail.com>
2017-12-12 13:59:28 +0100

    fixing instagram

--- app/jobs/insta_job.rb
+++ app/jobs/insta_job.rb
@@ -1,20 +1,22 @@
 class InstaJob < ApplicationJob
   queue_as :default
 
-  def perform(username, user_id)
-    @user = user_id
+  def perform(username, current_user, provider)
+    @provider = provider
+    @user = current_user
+    @username = username
     doc = RubyInstagramScraper.get_user_media_nodes(username)
     data = doc.to_s.scan(/(?<=display_src"=>")[^"]+/)
-
     if data == nil
       redirect_to root_path
     else
       data.each do |photo|
         Result.create!(
-          user: user_id,
+          user: @user,
           name: @username,
           category: "photo",
-          picture: photo
+          picture: photo,
+          provider: @provider
       )
       end
     end
