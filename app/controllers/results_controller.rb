13fcbfd9bf1e6b30279370fea7fbb45c0c8eb13b
G VDH <vanderhaegen.gauthier@gmail.com>
2017-12-12 09:56:34 +0100

    fix minor bugs

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -8,9 +8,7 @@ class ResultsController < ApplicationController
 
     @provider = params[:provider]
     @results = policy_scope(Result).select { |r| r.provider.name == @provider }
-    fail
-    if @provider.nil? || @results.size < 1
-      redirect_to new_provider_path(params[:provider])
+
     #.where(provider: provider).order(created_at: :desc)
     if @loaded_provider.nil?
       return redirect_to new_provider_path(params[:provider])
@@ -48,4 +46,5 @@ class ResultsController < ApplicationController
     @provider = params[:provider]
     @results = policy_scope(Result)
   end
+
 end
