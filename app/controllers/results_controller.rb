e040c3fb1284ba2a1212eb5c9d5be2686dcead89
Lucie Keunen <lucie@keunen.net>
2017-12-13 15:50:42 +0100

    Filtering with params and not with JS (snif snif)

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -22,9 +22,40 @@ class ResultsController < ApplicationController
   def load_results
     @provider = params[:provider]
     @results = policy_scope(Result).select { |r| r.provider.name == @provider }
-    @paginatable_array = Kaminari.paginate_array(@results).page(params[:page])
-    @results = @paginatable_array
+    # @paginatable_array = Kaminari.paginate_array(@results).page(params[:page])
+    # @results = @paginatable_array
     @loaded_provider = Provider.where(name: params[:provider], user: current_user).last
+
+    # check_results_size ??
+
+    @category = params[:category]
+    if params[:category] == 'photo'
+      @results = @results.select { |r| r.category == "photo" }
+    elsif params[:category] == 'post'
+      @results = @results.select { |r| r.category == "post" }
+    elsif params[:category] == 'video'
+      @results = @results.select { |r| r.category == "video" }
+    elsif params[:category] == 'page'
+      @results = @results.select { |r| r.category == "page" }
+    else
+      @results
+    end
+
+    @privacy = params[:privacy]
+    if params[:privacy] == 'public'
+      @results = @results.select { |r| r.privacy == "EVERYONE"}
+    elsif params[:privacy] == 'friends'
+      @results = @results.select { |r| r.privacy == "ALL_FRIENDS"}
+    elsif params[:privacy] == 'myself'
+      @results = @results.select { |r| r.privacy == "SELF"}
+    else
+      @results
+    end
+
+    # @order = params[:order]
+    # if params[:order] == 'reverse'
+    #   @results = @results.order("created_at DESC")
+    # end
   end
 
 end
